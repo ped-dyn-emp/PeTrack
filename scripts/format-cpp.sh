9cb1e268d879879f48f7ee42bfbaf5aeef42c0a4
d.kilic <d.kilic@fz-juelich.de>
2024-01-31 11:13:09 +0100

    Restructure physical layout of source files
    - no split between include/src anymore
    - more directories grouping files by their function
    - CMakeLists.txt setup to support file lists in each subdir

--- scripts/format-cpp.sh
+++ scripts/format-cpp.sh
@@ -4,7 +4,6 @@ set -e
 
 origin=$(dirname "$(readlink -f "$0")")
 files=$(find "$(readlink -f ${origin}/../src)" -type f -regex "^.*\.\(hpp\|cpp\|h\|c\)$")
-files+=$'\n'$(find "$(readlink -f ${origin}/../include)" -type f -regex "^.*\.\(hpp\|cpp\|h\|c\)$")
 files+=$'\n'$(find "$(readlink -f ${origin}/../tests)" -type f -regex "^.*\.\(hpp\|cpp\|h\|c\)$")
 
 echo "${files}" | parallel clang-format-14 -i
